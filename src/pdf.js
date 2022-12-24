183e3027e2bb13aecec619b013346a336aff1da3
javier950107 <franciscojavier950107@gmail.com>
2022-12-24 03:16:15 -0600

    Done :E

--- src/pdf.js
+++ src/pdf.js
@@ -1,28 +1,32 @@
-const ILovePDFApi = require('@ilovepdf/ilovepdf-nodejs')
-const instance = new ILovePDFApi(process.env.PUBLIC_KEY, process.env.SECRET_KEY)
+const ILovePDFApi = require('@ilovepdf/ilovepdf-nodejs');
+const ILovePDFFile = require('@ilovepdf/ilovepdf-nodejs/ILovePDFFile');
 const fs = require('fs')
-const imgToPDF = require('image-to-pdf')
+const path = require('path')
 
 async function callPdf(){
-    const pages = [
-        "/home/iscoru95/Documentos/Developer/webscrapper/WebScrapper/output/picture0.jpg", // path to the image
-        "data:image/jpg;base64,iVBORw...", // base64
-        fs.readFileSync('/home/iscoru95/Documentos/Developer/webscrapper/WebScrapper/output/picture0.jpg') // Buffer
-    ]
+    let namesJpg =[]
 
-    imgToPDF(pages, imgToPDF.sizes.A4)
-    .pipe(fs.createWriteStream('output.pdf'))
-    /*try {
+    try {
+        const instance = new ILovePDFApi(process.env.PUBLIC_KEY, process.env.SECRET_KEY)
         let task = instance.newTask('imagepdf')
         await task.start()
-        await task.addFile('output/picture0.jpg')
-        await task.process()
-        const data = await task.download()
-        fs.writeFileSync('output/test.pdf', data) 
+
+        namesJpg = fs.readdirSync('output')
+
+        console.log('Adding pictures')
+        for (let i = 0; i < namesJpg.length; i++) {
+            const file = new ILovePDFFile(path.resolve(__dirname,`../output/${namesJpg[i]}`))
+            await task.addFile(file)
+        }
+        
+        await task.process({margin: 0, pagesize: 'A4'});
+        const data = await task.download();
+        fs.writeFileSync('output/example.pdf', data)
+        console.log('Done PDF!')
 
     } catch (error) {
         console.log(error)
-    }*/
+    }
 }
 
 module.exports = {callPdf}
